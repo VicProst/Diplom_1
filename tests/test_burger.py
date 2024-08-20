f9331d20aa8d5ee6f2ab308a9d479bfcc9218624
Victor Prostyakov <vicpro2142@yandex.ru>
2024-08-20 13:55:40 +0300

    new file: conftest.py; modified: tests/test_burger.py

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -2,6 +2,7 @@ from unittest.mock import Mock
 import pytest
 from praktikum.burger import Burger
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+from conftest import new_bun_mock, new_ingredient_mock
 
 
 class TestBurger:
@@ -38,11 +39,8 @@ class TestBurger:
 
     # Удаление ингредиента
     @pytest.mark.parametrize('ingredient_type', [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
-    def test_remove_ingredient_add_mock_ingredient_empty_line(self, ingredient_type):
-        ingredient_mock = Mock()
-        ingredient_mock.ingredient_type = ingredient_type
-        ingredient_mock.name = 'Spicy-X'
-        ingredient_mock.price = 1.5
+    def test_remove_ingredient_add_mock_ingredient_empty_line(self, ingredient_type, new_ingredient_mock):
+        ingredient_mock = new_ingredient_mock[0]
         burger = Burger()
         burger.add_ingredient(ingredient_mock)
         burger.remove_ingredient(0)
@@ -50,15 +48,9 @@ class TestBurger:
 
     # Перемещение ингридиента
     @pytest.mark.parametrize('ingredient_type', [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
-    def test_move_ingredient_add_two_mock_ingredients_ingredients_swapped(self, ingredient_type):
-        ingredient_one_mock = Mock()
-        ingredient_one_mock.ingredient_type = ingredient_type
-        ingredient_one_mock.name = 'Spicy-X'
-        ingredient_one_mock.price = 1.5
-        ingredient_two_mock = Mock()
-        ingredient_two_mock.ingredient_type = ingredient_type
-        ingredient_two_mock.name = 'Сыр с астероидной плесенью'
-        ingredient_two_mock.price = 5.5
+    def test_move_ingredient_add_two_mock_ingredients_ingredients_swapped(self, ingredient_type, new_ingredient_mock):
+        ingredient_one_mock = new_ingredient_mock[0]
+        ingredient_two_mock = new_ingredient_mock[1]
         burger = Burger()
         burger.add_ingredient(ingredient_one_mock)
         burger.add_ingredient(ingredient_two_mock)
@@ -67,16 +59,9 @@ class TestBurger:
 
     # Получение цены
     @pytest.mark.parametrize('ingredient_type', [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
-    def test_get_price_add_mock_bun_and_mock_ingredient_true(self, ingredient_type):
-        bun_mock = Mock()
-        bun_mock.name = 'Crunchie bun'
-        bun_mock.price = 12.5
-        bun_mock.get_price.return_value = bun_mock.price
-        ingredient_mock = Mock()
-        ingredient_mock.ingredient_type = ingredient_type
-        ingredient_mock.name = 'Spicy-X'
-        ingredient_mock.price = 1.5
-        ingredient_mock.get_price.return_value = ingredient_mock.price
+    def test_get_price_add_mock_bun_and_mock_ingredient_true(self, ingredient_type, new_bun_mock, new_ingredient_mock):
+        bun_mock = new_bun_mock
+        ingredient_mock = new_ingredient_mock[0]
         burger = Burger()
         burger.set_buns(bun_mock)
         burger.add_ingredient(ingredient_mock)
@@ -84,22 +69,11 @@ class TestBurger:
 
     # Получение чека
     @pytest.mark.parametrize('ingredient_type', [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
-    def test_get_receipt(self, ingredient_type):
-        bun_mock = Mock()
-        bun_mock.name = 'Crunchie bun'
-        bun_mock.price = 12.5
-        bun_mock.get_name.return_value = bun_mock.name
-        bun_mock.get_price.return_value = bun_mock.price
-        ingredient_mock = Mock()
-        ingredient_mock.ingredient_type = ingredient_type
-        ingredient_mock.name = 'Spicy-X'
-        ingredient_mock.price = 1.5
-        ingredient_mock.get_type.return_value = ingredient_mock.ingredient_type
-        ingredient_mock.get_name.return_value = ingredient_mock.name
-        ingredient_mock.get_price.return_value = ingredient_mock.price
+    def test_get_receipt_add_mock_bun_mock_ingredient_true(self, ingredient_type, new_bun_mock, new_ingredient_mock):
+        bun_mock = new_bun_mock
+        ingredient_mock = new_ingredient_mock[0]
         burger = Burger()
         burger.set_buns(bun_mock)
         burger.add_ingredient(ingredient_mock)
-        burger.get_price()
         assert burger.get_receipt() == (f'(==== Crunchie bun ====)\n= {ingredient_type.lower()} Spicy-X'
                                         f' =\n(==== Crunchie bun ====)\n\n''Price: 26.5')
